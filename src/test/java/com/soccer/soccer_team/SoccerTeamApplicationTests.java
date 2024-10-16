58e33f374fafa15102de033041dc1fc3c3a0a8ec
Duda <maria.oliveira@zup.com.br>
2024-10-16 15:28:14 -0300

    add files of project

---
+++ src/test/java/com/soccer/soccer_team/SoccerTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soccer.soccer_team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
