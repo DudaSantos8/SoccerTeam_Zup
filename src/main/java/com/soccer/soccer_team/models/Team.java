b18fdfcbe3d257c473b393f85247abf787b7e2ba
Duda <maria.oliveira@zup.com.br>
2024-10-16 15:26:52 -0300

    add files of models

---
+++ src/main/java/com/soccer/soccer_team/models/Team.java
@@ -0,0 +1,45 @@
+package com.soccer.soccer_team.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.List;
+
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private List<String> players;
+
+    public Team() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<String> players) {
+        this.players = players;
+    }
+
+}
