5e528117a5c7e5a69d39eeee5923535afde4055b
Duda <maria.oliveira@zup.com.br>
2024-10-16 15:27:15 -0300

    add files of repositories

---
+++ src/main/java/com/soccer/soccer_team/repositories/TeamRepository.java
@@ -0,0 +1,9 @@
+package com.soccer.soccer_team.repositories;
+
+import com.soccer.soccer_team.models.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
+}
